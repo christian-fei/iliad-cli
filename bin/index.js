ecf32bfc5adbf1c5a2d2fb751fd905460d5357b0
Christian Fei <crifei93@gmail.com>
2018-09-27 19:42:32 +0200

    log instead of throwing

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 const {login, formatFor} = require('..')
 async function main ({username, password} = {}, next) {
-  if (!username) throw new Error('missing username')
-  if (!password) throw new Error('missing password')
+  if (!username) return console.log('⛔️ missing username')
+  if (!password) return console.log('⛔️ missing password')
 
   return login({username, password})
   .then(next)
